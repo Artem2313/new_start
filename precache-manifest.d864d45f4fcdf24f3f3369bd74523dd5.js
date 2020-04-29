3dc75e9e1a769dbb9621e41db9dbb864ef9102db
Artem2313 <huselnykov@gmail.com>
2020-04-29 19:41:47 +0300

    Updates

---
+++ precache-manifest.d864d45f4fcdf24f3f3369bd74523dd5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "6715827d09f171b879d6",
+    "url": "/Artem2313/new_start/static/js/main.a1baf36c.chunk.js"
+  },
+  {
+    "revision": "45ac65065f108ab015a0",
+    "url": "/Artem2313/new_start/static/js/runtime~main.5d35d7b1.js"
+  },
+  {
+    "revision": "849c89147605b187c5c8",
+    "url": "/Artem2313/new_start/static/js/2.99e81d51.chunk.js"
+  },
+  {
+    "revision": "380c6022a5fa4203fd0ee01b39cf5512",
+    "url": "/Artem2313/new_start/index.html"
+  }
+];
