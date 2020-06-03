535462eb2ac65aa3dac9a4c349c0e2e674a72e8d
Artem2313 <huselnykov@gmail.com>
2020-06-03 18:22:37 +0300

    'new'

--- src/components/StepSelector/StepSelector.jsx
+++ src/components/StepSelector/StepSelector.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import Select from 'react-select';
 import * as timerActions from '../../redux/timerActions';
+import * as timerSelectors from '../../redux/timerSelectors';
 
 const options = [
   { value: 5, label: 5 },
@@ -9,9 +10,11 @@ const options = [
   { value: 15, label: 15 },
 ];
 
+const findOption = value => options.find(opt => opt.value === value);
+
 const StepSelector = ({ value, onChange }) => (
   <div style={{ width: 400 }}>
-    <Select options={options} value={value} onChange={onChange} />
+    <Select options={options} value={findOption(value)} onChange={onChange} />
   </div>
 );
 
@@ -20,7 +23,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  onChange: step => dispatch(timerActions.changeStep(step)),
+  onChange: step => dispatch(timerActions.changeStep(step.value)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(StepSelector);
